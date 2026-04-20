c9b68aee0cc77b332811906706e5f85776cd350e
Yasmin <yasmin.jobs33@gmail.com>
2026-04-20 17:35:14 -0300

    ajuste do caminho de schema.prisma

--- dist/controller/AdminController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AdminController = void 0;
-class AdminController {
-    async enter(request, response) {
-        return response.json({ message: "Bem-vindo admin!" });
-    }
-}
-exports.AdminController = AdminController;
--- dist/types/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRole = void 0;
-var UserRole;
-(function (UserRole) {
-    UserRole["USER"] = "USER";
-    UserRole["ADMIN"] = "ADMIN";
-})(UserRole || (exports.UserRole = UserRole = {}));
